5fcda74b3d52ef71df19c67729f6d076e53fe37b
Marina Garrido <vg6501@engie.com>
2024-12-09 23:49:57 +0100

    [ADD] Postgres configuration for Django

--- api/zexeltest/settings.py
+++ api/zexeltest/settings.py
@@ -106,8 +106,12 @@ WSGI_APPLICATION = 'zexeltest.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django_db',
+        'USER': 'django_user',
+        'PASSWORD': 'django_password',
+        'HOST': 'postgres',
+        'PORT': '5432'
     }
 }
 
