3c4700c9394372a71a1d7ff94df5c3872bae569c
Marina Garrido <vg6501@engie.com>
2025-01-02 22:15:15 +0100

    [UPD] Improvement validators

--- api/apps/payment/models.py
+++ api/apps/payment/models.py
@@ -30,36 +30,56 @@ class Payment(models.Model):
     def __str__(self):
         return f"{self.source_amount} {self.source_currency} to {self.target_amount} {self.target_currency}"
 
-    """Static method to validate country code (using https://countriesnow.space)"""
     @staticmethod
-    def is_valid_country(country_code):
-        url = "https://countriesnow.space/api/v0.1/countries/iso"
+    def get_valid_data():
+        url = "https://countriesnow.space/api/v0.1/countries/currency"
         try:
             response = requests.get(url)
             if response.status_code == 200:
                 data = response.json()["data"]
-                valid_iso_codes = [country["Iso2"] for country in data]
-                return country_code in valid_iso_codes
+                valid_iso_codes = [country["iso2"] for country in data]
+                valid_currency_codes = [country["currency"] for country in data]
+                return valid_iso_codes, valid_currency_codes
             else:
                 raise ValueError("Error: Not possible to check country code", code=response.status_code)
 
         except requests.exceptions.RequestException as e:
             raise ValueError("Error trying to connect with external API countriesnow: {e}")
     
-    """Static method to validate currency code (using https://countriesnow.space)"""
-    @staticmethod
-    def is_valid_currency(currency_code):
-        url = "https://countriesnow.space/api/v0.1/countries/currency"
-        try:
-            response = requests.get(url)
-            if response.status_code == 200:
-                data = response.json()["data"]
-                valid_currencies_codes = [country["currency"] for country in data]
-                return currency_code in valid_currencies_codes
-            else:
-                raise ValueError("Error: Not possible to check currency", code=response.status_code)
-        except requests.exceptions.RequestException as e:
-            raise ValueError("Error trying to connect with external API countriesnow: {e}")
+    # COMENTARIO 1: En un principio pense en usar metodos separados para obtener la información teniendo en cuenta la API de countriesno. Dicho de otro modo,
+    # me parecía más limpio. Sin embargo, me he dado cuenta que usando el endpoint especificado en is_valid_currency podemos obtener todos los datos que necesitamos.
+    # Además, he refactorizado el código de modo que hacemos una funcion en la que obtenemos los datos necesarios y simplemente realizamos una comprobación. 
+    # De ese modo, no realizamos un número elevado de llamadas (evitando que en un futuro nos bloqueen por protecciones de seguridad que tenga la propia API)
+    # """Static method to validate country code (using https://countriesnow.space)"""
+    # @staticmethod
+    # def is_valid_country(country_code):
+    #     url = "https://countriesnow.space/api/v0.1/countries/iso"
+    #     try:
+    #         response = requests.get(url)
+    #         if response.status_code == 200:
+    #             data = response.json()["data"]
+    #             valid_iso_codes = [country["Iso2"] for country in data]
+    #             return country_code in valid_iso_codes
+    #         else:
+    #             raise ValueError("Error: Not possible to check country code", code=response.status_code)
+
+    #     except requests.exceptions.RequestException as e:
+    #         raise ValueError("Error trying to connect with external API countriesnow: {e}")
+    
+    # """Static method to validate currency code (using https://countriesnow.space)"""
+    # @staticmethod
+    # def is_valid_currency(currency_code):
+    #     url = "https://countriesnow.space/api/v0.1/countries/currency"
+    #     try:
+    #         response = requests.get(url)
+    #         if response.status_code == 200:
+    #             data = response.json()["data"]
+    #             valid_currencies_codes = [country["currency"] for country in data]
+    #             return currency_code in valid_currencies_codes
+    #         else:
+    #             raise ValueError("Error: Not possible to check currency", code=response.status_code)
+    #     except requests.exceptions.RequestException as e:
+    #         raise ValueError("Error trying to connect with external API countriesnow: {e}")
 
     """ Custom validations for Payment model"""
     def clean(self):
@@ -70,19 +90,35 @@ class Payment(models.Model):
                 ValidationError({"target_amount": "Monto should be a positive value"}, code=406)
             ])
         
-        # Validation: Country codes
-        if not self.is_valid_country(self.source_country) or not self.is_valid_country(self.target_country):
+        valid_iso_codes, valid_currency_codes = self.get_valid_data()
+        if not self.source_country in valid_iso_codes or not self.target_country in valid_iso_codes:
             raise ValidationError([
                 ValidationError({"source_country": "{self.source_country} is not a right country ISO code"}, code=406),
                 ValidationError({"target_country": "{self.target_country} is not a right country ISO code"}, code=406)
             ])
         
-        # Validation: Currency codes
-        if not self.is_valid_currency(self.source_currency) or not self.is_valid_currency(self.target_currency):
+        if not self.source_currency in valid_currency_codes or not self.target_currency in valid_currency_codes:
             raise ValidationError([
-                ValidationError({"source_currency": "{self.source_currency} is not a right country ISO code"}, code=406),
-                ValidationError({"target_currency": "{self.target_currency} is not a right country ISO code"}, code=406)
+                ValidationError({"source_country": "{self.source_country} is not a right country ISO code"}, code=406),
+                ValidationError({"target_country": "{self.target_country} is not a right country ISO code"}, code=406)
             ])
+        
+        # # COMENTARIO 2: (Comentario en RELACIÓN al COMENTARIO 1)Estas validaciones se realizarian si utilizaramos
+        # las funciones previamente marcadas.
+
+        # # Validation: Country codes
+        # if not self.is_valid_country(self.source_country) or not self.is_valid_country(self.target_country):
+        #     raise ValidationError([
+        #         ValidationError({"source_country": "{self.source_country} is not a right country ISO code"}, code=406),
+        #         ValidationError({"target_country": "{self.target_country} is not a right country ISO code"}, code=406)
+        #     ])
+        
+        # # Validation: Currency codes
+        # if not self.is_valid_currency(self.source_currency) or not self.is_valid_currency(self.target_currency):
+        #     raise ValidationError([
+        #         ValidationError({"source_currency": "{self.source_currency} is not a right country ISO code"}, code=406),
+        #         ValidationError({"target_currency": "{self.target_currency} is not a right country ISO code"}, code=406)
+        #     ])
 
         # Validation: Origin Country and Destination Country should be different
         if self.source_country == self.target_country:
